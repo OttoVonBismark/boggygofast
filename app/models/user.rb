cfa8911428da48e1da852e4f5c06024cfe583d18
Aaron Zaborac <aaron.zaborac@outlook.com>
2017-12-13 14:31:34 -0600

    Creates Users Controller and Model and adds functionality (including stubs for later work) to both.

---
+++ app/models/user.rb
@@ -0,0 +1,42 @@
+class User < ApplicationRecord
+    before_save :downcase_email
+
+    validates :name, presence: true, length: {maximum: 50}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+    has_secure_password
+    validates :password, presence: true, length: {minimum: 6}, allow_nil: true # allow_nil is true so folks can update their profiles without needing a password.
+
+    # Returns the hash digest of the given string.
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Returns a random token.
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def remember
+    end
+
+    def authenticated?(attribute_token)
+        digest = send("#{attribute}_digest")
+        return false if digest.nil?
+        BCrypt::Password.new(digest).is_password?(token)
+    end
+
+    def forget
+    end
+
+    
+
+    private
+
+    # Converts email to all lower-case
+    def downcase_email
+        self.email = email.downcase
+    end
+
+end
