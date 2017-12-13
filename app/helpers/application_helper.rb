7658c5dacbe2fc8f3b036f0f03fd80e5f74f0aca
Aaron Zaborac <aaron.zaborac@outlook.com>
2017-12-13 17:15:57 -0600

    Created frontend pages and css for Users signup. Need to create Sessions logic and seeds for testing. Need to write tests.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,12 @@ module ApplicationHelper
             page_title + " | " + base_title
         end
     end
+
+    # Sets button text where necessary.
+    def button_text(button_name = '')
+        button_base_name = "Proceed"
+        if button_name.empty?
+            button_base_name
+        end
+    end
 end
