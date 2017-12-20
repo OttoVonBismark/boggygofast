1ae230df806840db8d6ac830cc906cc0378afbcf
Aaron Zaborac <aaron.zaborac@outlook.com>
2017-12-20 13:51:14 -0600

    Creates Game model and controller.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220173850) do
+ActiveRecord::Schema.define(version: 20171220181330) do
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_games_on_name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
