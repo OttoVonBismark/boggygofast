57ce4f02f91531b4a78ec90ff1f91f0c1b3c5025
Aaron Zaborac <aaron.zaborac@outlook.com>
2017-12-27 16:48:25 -0600

    Tidies up code a bit. Adds style to the games index. This branch is now finished.

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,10 +3,5 @@ class Game < ApplicationRecord
     friendly_id :param, use: [:finders, :slugged]
 
     validates_presence_of :slug
-    
-    # This allows 'slug' to be used to create friendly urls such as ~/games/metroid instead of ~/games/1
-    # def to_param
-    #     slug
-    # end
 
 end
