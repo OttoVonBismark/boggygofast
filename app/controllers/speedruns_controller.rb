5bb8e01f5e0a08a228b28524977b2e05f910bb98
Aaron Zaborac <aaron.zaborac@outlook.com>
2018-06-06 17:11:21 -0400

    Does all the things! Adds active relations extensions so that runs can be pulled based on criteria while maintaining order. Speedrun model handles background run preparation tasks such as grabbing the user name associated with user_id. Adds speedrun index which has no direct links yet. Speedrun leaderboard formatting is ugly and due for CSS. Leaderboards controller may get nixed due to redundancy.

--- app/controllers/speedruns_controller.rb
+++ app/controllers/speedruns_controller.rb
@@ -7,7 +7,6 @@ class SpeedrunsController < ApplicationController
     end
 
     def index
-        @speedruns = Speedrun.paginate(page: params[:page])
     end
 
     def new
@@ -18,7 +17,7 @@ class SpeedrunsController < ApplicationController
         @speedrun = @game.speedruns.new(speedrun_params)
         if @speedrun.save
             flash[:info] = "Run submitted. It will appear once an admin has verified it."
-            redirect_to @speedruns
+            redirect_to root_url
         else
             render 'new'
         end 
@@ -40,6 +39,16 @@ class SpeedrunsController < ApplicationController
         Speedrun.find(params[:id]).destroy
         flash[:success] = "Run deleted"
         redirect_to games_index # Update me
+    end
+
+    def retrieve_runs_by_category(rcid)
+        Speedrun.where('runcat_id == ?', rcid).to_a
+        # Speedrun.find_by_sql([<<-SQL, runcat_id: cat]) 
+        #   SELECT user_id, game_id, runcat_id, run_time_h, run_time_m, run_time_s, date_finished, run_notes FROM speedruns WHERE runcat_id = ?
+        #     ORDER BY run_time_h ASC
+        #     ORDER BY run_time_m ASC 
+        #     ORDER BY run_time_s ASC
+        #   SQL
     end
 
 
--- app/models/speedrun.rb
+++ app/models/speedrun.rb
@@ -1,13 +1,38 @@
 class Speedrun < ApplicationRecord
+
+  include ActiveRelationExtensions
+  
   require 'date'
   require 'active_support/core_ext/string'
 
   belongs_to :game
   belongs_to :user # This is mostly symbolic. Don't CREATE speedruns via @user. Do it with @game instead. Read and Destroy are fine though.
-  has_one :runcat
 
   validates_presence_of :date_finished
   validates :run_time_h, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 99}
   validates :run_time_m, :run_time_s, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 59}
+
+  def runner_name
+    runner = User.find_by_id(self.user_id)
+    runner_name = runner.name
+  end
+
+  def run_time
+    rt_h = self.run_time_h.to_s
+    
+    if self.run_time_m < 10
+      rt_m = "0" + self.run_time_m.to_s
+    else
+      rt_m = self.run_time_m.to_s
+    end
+
+    if self.run_time_s < 10
+      rt_s = "0" + self.run_time_s.to_s
+    else
+      rt_s = self.run_time_s.to_s
+    end
+
+    run_time = rt_h + ":" + rt_m + ":" + rt_s
+  end
 
 end
