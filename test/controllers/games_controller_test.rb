a8bc95eea5f452bcf35f27576f630fcd1b4025c7
Aaron Zaborac <aaron.zaborac@outlook.com>
2018-06-16 10:55:52 -0400

    Updates games and runcats controller tests.

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -18,10 +18,30 @@ class GamesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "List of Games | BoggyGoFast Speedrun Archive"
   end
 
+  test "should get new game page for admin users" do
+    log_in_as(@user)
+    assert @user.admin?
+    get new_game_path
+    assert_response :success
+    assert_select "title", "Create a New Game | BoggyGoFast Speedrun Archive"
+  end
+
+  test "should get show page" do
+    get game_path(@sonic)
+    assert_response :success
+  end
+  
   test "should redirect non-admins from new game page" do
     log_in_as(@other_user)
     refute @other_user.admin?
     get new_game_path
+    assert_redirected_to root_url
+  end
+
+  test "should redirect non-admins from edit game page" do
+    log_in_as(@other_user)
+    refute @other_user.admin?
+    get edit_game_path(@sonic.slug)
     assert_redirected_to root_url
   end
 
--- test/controllers/runcats_controller_test.rb
+++ test/controllers/runcats_controller_test.rb
@@ -1,5 +1,25 @@
 require 'test_helper'
 
 class RuncatsControllerTest < ActionDispatch::IntegrationTest
-  
+    def setup
+        # Games
+        @sonic = games(:sonic)
+        @metroid = games(:metroid)
+
+        # Runcats
+        @sonic_anyperc = runcats(:sonic_anyperc)
+        @sonic_100perc = runcats(:sonic_100perc)
+        @metroid_anyperc = runcats(:metroid_anyperc)
+        @metroid_100perc = runcats(:metroid_100perc)
+
+        # Users
+        @user = users(:michael)
+        @other_user = users(:archer)
+    end
+
+    test "should get index" do
+        get runcats_path(@sonic.slug)
+        assert_response :success
+        assert_select "title", "Categories for Sonic Mania | BoggyGoFast Speedrun Archive"
+    end
 end
