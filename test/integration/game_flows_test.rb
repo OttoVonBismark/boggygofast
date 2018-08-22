69d9200a427e1de9d93732dd3cafdaade71fb190
Aaron Zaborac <aaron.zaborac@outlook.com>
2018-08-22 14:42:01 -0400

    Switches link destinations around in the Games Index so that clicking a game's name will take you to the speedrun leaderboards instaed of the game info page, replacing the 'Runs' link with said info page link. Adds a check in new Speedrun creation to make sure categories for a game exist before a user can submit times. Updates seeds.rb to include game info, which was previously absent. Updates tests to reflect URL updates.

--- test/integration/game_flows_test.rb
+++ test/integration/game_flows_test.rb
@@ -16,8 +16,8 @@ class GameFlowsTest < ActionDispatch::IntegrationTest
     assert_template 'games/index'
     game_collection = Game.all
     game_collection.each do |game|
-      assert_select 'a[href=?]', game_path(game), text: game.name
-      assert_select 'a[href=?]', speedruns_path(game.slug), text: "| Runs"
+      assert_select 'a[href=?]', speedruns_path(game.slug), text: game.name
+      assert_select 'a[href=?]', game_path(game), text: "| Game Info"
       assert_select 'a[href=?]', runcats_path(game.slug), text: "| Categories"
       unless @admin.admin?
         assert_select 'a[href=?]', new_game_runcat_path(game.slug), text: "| New Category"
@@ -37,8 +37,8 @@ class GameFlowsTest < ActionDispatch::IntegrationTest
     assert_template 'games/index'
     game_collection = Game.all
     game_collection.each do |game|
-      assert_select 'a[href=?]', game_path(game), text: game.name
-      assert_select 'a[href=?]', speedruns_path(game.slug), text: "| Runs"
+      assert_select 'a[href=?]', speedruns_path(game.slug), text: game.name
+      assert_select 'a[href=?]', game_path(game), text: "| Game Info"
       assert_select 'a[href=?]', runcats_path(game.slug), text: "| Categories"
     end
     assert_no_difference 'Game.count' do
